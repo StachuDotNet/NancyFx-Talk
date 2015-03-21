fc3a78539e7bdda79aed16293205bd0c49ca8836
skorick <skorick@academyone.com>
2015-03-21 06:55:58 -0400

    minor edits

--- Demos/End/First Look/HomeModule.cs
+++ Demos/End/First Look/HomeModule.cs
@@ -7,12 +7,103 @@ namespace End
         public HomeModule()
         {
             Get["/"] = _ => "Hello, from Nancy";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             Get["/funcEquivalent"] = FuncEquivalent;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             Get["/{capturedValue}"] = _ => "Value captured in route: " + _.capturedValue;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             Get["/htmlPage"] = _ => View["index"];
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         // This is equivalent to _ => "Hello, from Nancy".
         // This is a C# feature, and is not specific to Nancy at all!
